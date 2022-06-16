f390ca2b973da1e04f2f205264cc5f388dee3210
Anjana Nair <107626169+nairanjananl@users.noreply.github.com>
2022-06-16 18:55:44 +0200

    Update streamlit_app.py
    Fix error on line for multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Let's pick a pick list here so they can pick a fruit they want
-streamlist.multiselect("Pick some fruits : " , list(my_fruit_list.index))
+streamlist.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 #display the table on the page
 streamlit.dataframe(my_fruit_list)
