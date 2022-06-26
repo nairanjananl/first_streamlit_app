2e94877a90f906548103b67f54d3f2ceb6399c0a
Anjana Nair <107626169+nairanjananl@users.noreply.github.com>
2022-06-26 13:01:06 +0200

    Update streamlit_app.py
    syntax error inset_row_snowflake() function

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ if streamlit.button('Get Fruit Load List'):
 streamlit.stop()
   
 #Allow the end-user to add a fruit to the list
-def insert_row_snowflake(new_fruit)
+def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
          my_cur.executive("insert into fruit_load_list values ('from streamlit')")
          retrun "Thanks for adding " + new_fruit
